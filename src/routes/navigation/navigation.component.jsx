e701ab3256c137082a5e6843b3dbc1d831983cbc
Pham Van Hoan <phamvanhoan@luvina.net>
2023-11-20 09:32:11 +0700

    [Update] Add css for navigation bar

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,27 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
+import "./navigation.style.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to={"/"}>
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to={"/shop"}>
+            SHOP
+          </Link>
+        </div>
+        {/* <h1>I am the navigation bar</h1> */}
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
