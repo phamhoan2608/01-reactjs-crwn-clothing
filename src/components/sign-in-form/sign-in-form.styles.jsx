29a55ccf5fb43ce0e34d2f900e289a8be194e3b7
Pham Van Hoan <phamvanhoan@luvina.net>
2023-12-06 13:58:56 +0700

    Fix bug

---
+++ src/components/sign-in-form/sign-in-form.styles.jsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const SignInBtns = styled.div`
+  display: flex;
+  column-gap: 20px;
+`;
--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,6 +1,6 @@
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component.jsx";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component.jsx";
-import "./authentication.component.jsx";
+
 import { Container } from "./authentication.styles.jsx";
 
 const Authentication = () => {
