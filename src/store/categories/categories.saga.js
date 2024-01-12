84df889032a0a3aa0766a6500cfb34a4c7a61fe2
Pham Van Hoan <phamvanhoan@luvina.net>
2024-01-12 17:39:15 +0700

    [section-17] Add redux-saga

---
+++ src/store/categories/categories.saga.js
@@ -0,0 +1,21 @@
+import { takeLatest, all, call, put, take } from "redux-saga/effects";
+import { fetchCategoriesFailed, fetchCategoriesSuccess } from "./categories.action";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import { CATEGORIES_ACTION_TYPES } from "./categories.types";
+
+export function* fetchCategoriesAsync() {
+  try {
+    const categoryArray = yield call(getCategoriesAndDocuments, "categories");
+    yield put(fetchCategoriesSuccess(categoryArray));
+  } catch (error) {
+    yield put(fetchCategoriesFailed(error));
+  }
+}
+
+export function* onFetchCategories() {
+  yield takeLatest(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync);
+}
+
+export function* categoriesSaga() {
+  yield all([call(onFetchCategories)]);
+}
